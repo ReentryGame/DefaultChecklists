29f7879ec00f5a6b80be5d43f5f31c9d744b229b
Martin Sundhaug <martinsundhaug@gmail.com>
2020-01-02 22:54:47 +0100

    Added json-prettifier

---
+++ tools/json-prettifier/prettifier.py
@@ -0,0 +1,6 @@
+import json, glob
+
+for checklist in glob.glob('**/*.json', recursive=True):
+    print(checklist)
+    j = json.load(open(checklist))
+    open(checklist, 'w').write(json.dumps(j, indent=4, sort_keys=True))
