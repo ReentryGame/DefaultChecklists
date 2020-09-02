2ddeeb4c25ecf2d74731b9192df9b1d67ee5fa7f
Martin Sundhaug <martinsundhaug@gmail.com>
2020-09-02 20:28:34 +0200

    prettifier: Skip non-checklists

--- tools/json-prettifier/prettifier.py
+++ tools/json-prettifier/prettifier.py
@@ -1,6 +1,7 @@
 import json, glob
 
 for checklist in glob.glob('**/*.json', recursive=True):
+    if not checklist.endswith('checklist.json'): continue
     print(checklist)
     j = json.load(open(checklist))
     open(checklist, 'w').write(json.dumps(j, indent=4, sort_keys=True))
