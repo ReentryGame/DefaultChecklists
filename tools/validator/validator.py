1000291d3c1d7545a9fc80de66c0ac472c071cb8
Martin Sundhaug <martinsundhaug@gmail.com>
2020-10-26 20:04:39 +0100

    Remove unused if's and blank spaces

--- tools/validator/validator.py
+++ tools/validator/validator.py
@@ -47,34 +47,31 @@ for checklist in glob.glob('*/*/checklist.json'):
             print(checklist, 'MAIN', main_key, 'Key has wrong type')
             file_ok = False
             all_valid = False
-    if 'Steps' in j.keys():
-        for step in j['Steps']:
-            if step is None:
-                # TODO: Ask @petriw if this is valid
-                continue
-            for step_key in step_key_fields:
-                if step_key not in step.keys():
-                    print(checklist, 'STEP', step_key, 'MISSING')
-                    file_ok = False
-                    all_valid = False
-            for step_key in step.keys():
-                if step_key not in step_key_fields:
-                    print(checklist, 'STEP', step_key, 'Invalid key')
-                    file_ok = False
-                    all_valid = False
-                # TODO: Add type-validation for step-keys. One issue is that NULL appears to be a valid value for Text
-    if 'Images' in j.keys():
-        for image in j['Images']:
-            for image_key in image_key_fields:
-                if image_key not in image.keys():
-                    print(checklist, 'IMAGE', image_key, 'MISSING')
-                    file_ok = False
-                    all_valid = False
-                if type(image[image_key]) != image_key_types[image_key]:
-                    print(checklist, 'IMAGE', image_key, 'Invalid key')
-                    file_ok = False
-                    all_valid = False
+    for step in j['Steps']:
+        if step is None:
+            # TODO: Ask @petriw if this is valid
+            continue
+        for step_key in step_key_fields:
+            if step_key not in step.keys():
+                print(checklist, 'STEP', step_key, 'MISSING')
+                file_ok = False
+                all_valid = False
+        for step_key in step.keys():
+            if step_key not in step_key_fields:
+                print(checklist, 'STEP', step_key, 'Invalid key')
+                file_ok = False
+                all_valid = False
+            # TODO: Add type-validation for step-keys. One issue is that NULL appears to be a valid value for Text
+    for image in j['Images']:
+        for image_key in image_key_fields:
+            if image_key not in image.keys():
+                print(checklist, 'IMAGE', image_key, 'MISSING')
+                file_ok = False
+                all_valid = False
+            if type(image[image_key]) != image_key_types[image_key]:
+                print(checklist, 'IMAGE', image_key, 'Invalid key')
+                file_ok = False
+                all_valid = False
 
-                    
+
 sys.exit(0 if all_valid else -1)
-            
