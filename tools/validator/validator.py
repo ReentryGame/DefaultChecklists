ad8bcf085a9b9d0ff0bbd6065dd4ee3c8ede321d
Martin Sundhaug <martinsundhaug@gmail.com>
2023-12-13 03:56:57 +0100

    Attempt to fix the validator

--- tools/validator/validator.py
+++ tools/validator/validator.py
@@ -19,6 +19,10 @@ main_key_fields = [
     'AutoGenerate'
 ]
 
+main_key_fields_ignorable = [
+    'AutoGenerate'
+]
+
 main_key_types = {
     'checklistText': str,
     'Group': str,
@@ -32,7 +36,11 @@ main_key_types = {
     'AutoGenerate': bool
 }
 
-step_key_fields = ['Program', 'Type', 'SetID', 'ToPosID', 'Text']
+step_key_fields = ['Program', 'Type', 'SetID', 'ToPosID', 'Text', 'Description']
+
+step_key_fields_ignorable = ['Description']
+
+# TODO: step_key_types
 
 image_key_fields = ['Name', 'Position', 'Size']
 
@@ -51,12 +59,15 @@ for checklist in glob.glob('*/*/checklist.json'):
     file_ok = True
     for main_key in main_key_fields:
         if main_key not in j.keys():
-            print(checklist, 'MAIN', main_key, 'MISSING')
-            file_ok = False
-            all_valid = False
+            if main_key in main_key_fields_ignorable:
+                print(checklist, 'MAIN', main_key, 'MISSING (OPTIONAL)')
+            else:
+                print(checklist, 'MAIN', main_key, 'MISSING')
+                file_ok = False
+                all_valid = False
     for main_key in j.keys():
         if main_key not in main_key_fields:
-            print(checklist, 'MAIN', main_key, 'Invalid key')
+            print(checklist, 'MAIN', main_key, 'Invalid (unknown) key')
             file_ok = False
             all_valid = False
         if type(j[main_key]) != main_key_types[main_key]:
@@ -69,19 +80,23 @@ for checklist in glob.glob('*/*/checklist.json'):
             continue
         for step_key in step_key_fields:
             if step_key not in step.keys():
-                print(checklist, 'STEP', step_key, 'MISSING')
-                file_ok = False
-                all_valid = False
+                if step_key in step_key_fields_ignorable:
+                    print(checklist, 'STEP', step_key, 'MISSING (OPTIONAL)')
+                else:
+                    print(checklist, 'STEP', step_key, 'MISSING')
+                    file_ok = False
+                    all_valid = False
             if step_key not in step_key_fields:
-                print(checklist, 'STEP', step_key, 'Invalid key')
+                print(checklist, 'STEP', step_key, 'Invalid (unknown) key')
                 file_ok = False
                 all_valid = False
         for step_key in step.keys():
-            if step_key not in step_key_fields:
-                print(checklist, 'STEP', step_key, 'Key has wrong type')
-                file_ok = False
-                all_valid = False
             # TODO: Add type-validation for step-keys. One issue is that NULL appears to be a valid value for Text
+            # if type(step_key) not in step_key_types[step_key]:
+            #     print(checklist, 'STEP', step_key, 'Key has wrong type')
+            #     file_ok = False
+            #     all_valid = False
+            pass
     for image in j['Images']:
         for image_key in image_key_fields:
             if image_key not in image.keys():
@@ -89,7 +104,7 @@ for checklist in glob.glob('*/*/checklist.json'):
                 file_ok = False
                 all_valid = False
             if image_key not in image_key_fields:
-                print(checklist, 'IMAGE', image_key, 'Invalid key')
+                print(checklist, 'IMAGE', image_key, 'Invalid (unknown) key')
                 file_ok = False
                 all_valid = False
             if type(image[image_key]) != image_key_types[image_key]:
@@ -105,7 +120,11 @@ for checklist in glob.glob('*/*/checklist.json'):
             print(checklist, 'IMAGE', image_path, 'File not found')
             file_ok = False
             all_valid = False
-        with Image.open(image_path) as im:
-            pass  # ATM, just "this is a valid image" is enough
+        try:
+            with Image.open(image_path) as im:
+                pass  # ATM, just "this is a valid image" is enough
+        except:
+            file_ok = False
+            all_valid = False
 
 sys.exit(0 if all_valid else -1)
